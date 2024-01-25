2571a9b0901c288d83553b7e98260defbf37433a
mitou <xmitoux@gmail.com>
2024-01-25 23:13:07 +0900

    最初の画像2枚が消える不具合を修正
    全件取得時のサンプル削除処理が不要になったのに残っていた

--- src/utils/dom-control.ts
+++ src/utils/dom-control.ts
@@ -1,17 +1,9 @@
 export const getAllTagTdElements = (): HTMLTableCellElement[] => {
-    const tags = Array.from(document.querySelectorAll<HTMLTableCellElement>('td:has(code)'));
-    // 1つ目はサンプルなので消す
-    tags.shift();
-
-    return tags;
+    return Array.from(document.querySelectorAll<HTMLTableCellElement>('td:has(code)'));
 };
 
 export const getAllImageTdElements = (): HTMLTableCellElement[] => {
-    const images = Array.from(document.querySelectorAll<HTMLTableCellElement>('td:has(img)'));
-    // 1つ目はサンプルなので消す
-    images.shift();
-
-    return images;
+    return Array.from(document.querySelectorAll<HTMLTableCellElement>('td:has(img)'));
 };
 
 export const rearrangeImages = (imagesPerRow: number) => {
