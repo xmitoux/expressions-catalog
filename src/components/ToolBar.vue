34ec2fda7c2e28dc501319ae3e7f72d7a28b01d3
mitou <xmitoux@gmail.com>
2024-01-25 21:38:27 +0900

    1行あたりの表示画像数変更機能を実装

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,56 +1,72 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import {
     ElCheckboxButton,
     ElCheckboxGroup,
     ElContainer,
     ElHeader,
     ElIcon,
+    ElInputNumber,
     ElMain,
+    ElRow,
     ElScrollbar,
 } from 'element-plus';
 import { Box, Paperclip, Star } from '@element-plus/icons-vue';
 import { filterPopularTags } from '@/content-scripts/filterPopularTags';
+import { rearrangeImages } from '@/utils/dom-control';
+
+const imagesPerRow = ref(5);
+
+onMounted(() => rearrangeImages(imagesPerRow.value));
 
 const checkboxGroup = ref([]);
 
 const onCheckChagend = () => {
     filterPopularTags(checkboxGroup.value.some((checkLabel) => checkLabel === 'popular'));
+    rearrangeImages(imagesPerRow.value);
+};
+
+const onImagesPerRowChange = () => {
+    rearrangeImages(imagesPerRow.value);
 };
 </script>
 
 <template>
     <ElContainer>
-        <ElHeader class="header">
-            <ElCheckboxGroup v-model="checkboxGroup" size="large" @change="onCheckChagend">
-                <ElCheckboxButton label="popular">
-                    <ElIcon :size="20"><Box /></ElIcon>
-                </ElCheckboxButton>
-                <ElCheckboxButton label="star">
-                    <ElIcon :size="20"><Star /></ElIcon>
-                </ElCheckboxButton>
-                <ElCheckboxButton label="clip">
-                    <ElIcon :size="20"><Paperclip /></ElIcon>
-                </ElCheckboxButton>
-            </ElCheckboxGroup>
+        <ElHeader>
+            <ElRow align="middle" justify="center">
+                <ElCheckboxGroup
+                    class="pt-2"
+                    v-model="checkboxGroup"
+                    size="large"
+                    @change="onCheckChagend"
+                >
+                    <ElCheckboxButton label="popular">
+                        <ElIcon :size="15"><Box /></ElIcon>
+                    </ElCheckboxButton>
+                    <ElCheckboxButton label="star">
+                        <ElIcon :size="15"><Star /></ElIcon>
+                    </ElCheckboxButton>
+                    <ElCheckboxButton label="clip">
+                        <ElIcon :size="15"><Paperclip /></ElIcon>
+                    </ElCheckboxButton>
+                </ElCheckboxGroup>
+
+                <ElInputNumber
+                    v-model="imagesPerRow"
+                    class="mx-2"
+                    controls-position="right"
+                    :min="1"
+                    :max="10"
+                    @change="onImagesPerRowChange"
+                />
+            </ElRow>
         </ElHeader>
 
-        <ElMain class="content">
+        <ElMain class="p-0">
             <ElScrollbar height="100vh">
                 <slot></slot>
             </ElScrollbar>
         </ElMain>
     </ElContainer>
 </template>
-
-<style scoped>
-.header {
-    text-align: center;
-    margin-top: 8px;
-    height: 56px;
-}
-
-.content {
-    padding: 0;
-}
-</style>
--- src/utils/dom-control.ts
+++ src/utils/dom-control.ts
@@ -13,3 +13,33 @@ export const getAllImageTdElements = (): HTMLTableCellElement[] => {
 
     return images;
 };
+
+export const rearrangeImages = (imagesPerRow: number) => {
+    const filterPredicate = (td: HTMLTableCellElement) => td.style.display === '';
+    const tagTDs = [...getAllTagTdElements()].filter(filterPredicate);
+    const imageTDs = [...getAllImageTdElements()].filter(filterPredicate);
+
+    const newRows: HTMLElement[] = [];
+
+    // 指定されたカラム数ごとにtdを分割し、新しいtrを作成
+    for (let i = 0; i < tagTDs.length; i += imagesPerRow) {
+        const tagTR = document.createElement('tr');
+        const imageTR = document.createElement('tr');
+
+        // タグ行と画像行を処理
+        tagTDs.slice(i, i + imagesPerRow).forEach((td) => {
+            tagTR.appendChild(td);
+        });
+        imageTDs.slice(i, i + imagesPerRow).forEach((td) => {
+            imageTR.appendChild(td);
+        });
+
+        newRows.push(tagTR, imageTR);
+    }
+
+    const table = document.querySelector('table')!;
+    table.innerHTML = '';
+    newRows.forEach((tr) => {
+        table.appendChild(tr);
+    });
+};
