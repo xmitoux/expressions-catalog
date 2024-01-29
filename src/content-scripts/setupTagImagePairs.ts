d105c9c70c3e95dd128877b1daf1631dcc20ceb9
mitou <xmitoux@gmail.com>
2024-01-29 22:10:07 +0900

    画像を対応するファイル名のassetsで置き換える処理を実装

--- src/content-scripts/setupTagImagePairs.ts
+++ src/content-scripts/setupTagImagePairs.ts
@@ -6,6 +6,35 @@ export const setupTagImagePairs = (): void => {
     const tagTdElements = getAllTagTdElements();
     const imageTdElements = getAllImageTdElements();
 
+    const replaceImagesWithAssets = (tagName: string, imageTd: HTMLTableCellElement) => {
+        const image = imageTd.querySelector<HTMLImageElement>('img')!;
+        const orgSrc = image.src;
+        image.onerror = () => (image.src = orgSrc);
+
+        // ファイル名に使用できない文字を含むタグは全角に置換
+        const convertToFullWidth = (str: string): string => {
+            const fullWidthChars: { [key: string]: string } = {
+                '\\': '＼',
+                '/': '／',
+                ':': '：',
+                '*': '＊',
+                '?': '？',
+                '"': '”',
+                '<': '＜',
+                '>': '＞',
+                '|': '｜',
+            };
+
+            return str
+                .split('')
+                .map((char) => fullWidthChars[char] || char)
+                .join('');
+        };
+
+        const replaceImageSrc = chrome.runtime.getURL(`/assets/${convertToFullWidth(tagName)}.png`);
+        image.src = replaceImageSrc;
+    };
+
     getStorage(({ tagsSettings }) => {
         const initTagImagePair = (tagTd: HTMLTableCellElement, imageTd: HTMLTableCellElement) => {
             // タグにidを設定
@@ -27,6 +56,8 @@ export const setupTagImagePairs = (): void => {
 
             const tagName = tagTd.id;
             const filterMarksJson = tagsSettings[tagName]?.filterMarksJson || '[]';
+            replaceImagesWithAssets(tagName, imageTd);
+
             setupImageContainer(tagTd, imageTd, filterMarksJson);
         });
     });
