9dfb9d9002b621cf6bac8eacc7430eddd87d8f68
mitou <xmitoux@gmail.com>
2024-01-30 21:33:49 +0900

    `^`を含むファイル名を読み込めない不具合を修正
    ファイル的には問題ないがURLではエンコードされるっぽい

--- src/content-scripts/setupTagImagePairs.ts
+++ src/content-scripts/setupTagImagePairs.ts
@@ -23,6 +23,7 @@ export const setupTagImagePairs = (): void => {
                 '<': '＜',
                 '>': '＞',
                 '|': '｜',
+                '^': '＾',
             };
 
             return str
